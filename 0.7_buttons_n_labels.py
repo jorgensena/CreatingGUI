a794984eeec4ffe2ac18edc3a84fd73907e4cde5
jorgensena <jorgensena@middleton.school.nz>
2022-03-16 17:33:38 +1300

    Create 0.7_buttons_n_labels.py

---
+++ 0.7_buttons_n_labels.py
@@ -0,0 +1,25 @@
+"""using multiple buttons"""
+
+from tkinter import *
+root = Tk()
+
+def press_button():
+    text = entry_text.get()
+    label_text.set(text)
+
+
+num = 0
+root.title("Changing label text")
+root.minsize(300, 50)
+
+entry_text = StringVar()
+enter_smth = Entry(textvariable=entry_text, font=("Arial", 15)).pack()
+
+button = Button(root, text="Press to show message", command=press_button)
+button.pack()
+
+label_text = StringVar()
+message = Label(root, textvariable=label_text, font=("Courier", 24, "bold"))
+message.pack()
+
+root.mainloop()
